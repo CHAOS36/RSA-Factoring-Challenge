bbdc39958c07e917d90c3e77026650cea98c0fbe
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-27 13:46:25 -0800

    .

--- factorization.py
+++ factorization.py
@@ -2,20 +2,20 @@
 import sys
 
 
-def primef(pipox):
-    if pipox <= 3:
-        return int(pipox)
-    if pipox % 2 == 0:
+def primef(n):
+    if n <= 3:
+        return int(n)
+    if n % 2 == 0:
         return 2
-    elif pipox % 3 == 0:
+    elif n % 3 == 0:
         return 3
     else:
-        for i in range(5, int((pipox)**0.5) + 1, 6):
-            if pipox % i == 0:
-                return int(i)
-            if pipox % (i + 2) == 0:
-                return primef(pipox/(i+2))
-    return int(pipox)
+        for pipox in range(5, int((n)**0.5) + 1, 6):
+            if n % pipox == 0:
+                return int(pipox)
+            if n % (pipox + 2) == 0:
+                return primef(n/(pipox+2))
+    return int(n)
 
 
 print(primef(int(sys.argv[1])))
